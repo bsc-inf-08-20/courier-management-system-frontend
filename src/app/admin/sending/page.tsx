88602efc2ae2387ca26211cbe4c984fa019c49fe
bsc-inf-08-20 <bsc-inf-08-20@unima.ac.mw>
2025-05-30 00:22:51 +0200

    base url changes

--- src/app/admin/sending/page.tsx
+++ src/app/admin/sending/page.tsx
@@ -82,7 +82,7 @@ export default function CreatePacketPage() {
       setLoading(true);
       try {
         const token = localStorage.getItem("token");
-        const res = await fetch("http://localhost:3001/users/me", {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/users/me`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -232,7 +232,7 @@ export default function CreatePacketPage() {
         status: "at_origin_hub",
       };
 
-      const res = await fetch("http://localhost:3001/packets", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/packets`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
